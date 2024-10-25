731c419c74b031d5781630057cc5d86172a27d93
AyoOluwa-Israel <izzyfresher@gmail.com>
2024-10-25 08:49:16 +0100

    feat: landing page #0

--- src/constants/homedata.ts
+++ src/constants/homedata.ts
@@ -26,7 +26,14 @@ import Logo4 from "@/assets/images/auditProcess/ey.png";
 import Logo5 from "@/assets/images/auditProcess/baba.png";
 import Logo6 from "@/assets/images/auditProcess/swipe.png";
 
+import Hero1 from "@/assets/images/auditProcess/ship.png";
+import Hero2 from "@/assets/images/auditProcess/topship.png";
+import Hero3 from "@/assets/images/auditProcess/send.png";
+import Hero4 from "@/assets/images/auditProcess/float.png";
+
 export const missonImages = [Logo1, Logo2, Logo3, Logo4, Logo5, Logo6];
+export const HeroLogo = [Hero1, Hero2, Hero3, Hero4];
+
 
 export const whyChoose = [
   {
