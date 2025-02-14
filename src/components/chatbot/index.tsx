5a9ffa97c63ab5b166e6bf0f601dadfd8c02c406
AyoOluwa-Israel <izzyfresher@gmail.com>
2025-02-14 12:59:25 +0100

    feat: website #0

--- src/components/chatbot/index.tsx
+++ src/components/chatbot/index.tsx
@@ -17,19 +17,19 @@ const ChatBot = () => {
       <AnimatePresence>
         {disabled && (
           <motion.div
-            key="overlay" 
+            key="overlay"
             className="bg-black bg-opacity-60 absolute top-0 left-0 w-[100%] h-[100%] backdrop-blur-[2px]  z-[100] rounded-lg flex items-center justify-center"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }} 
+            transition={{ duration: 0.3, ease: "easeInOut" }}
           >
             <motion.div
               className="sm:w-[60%] w-[90%] text-center bg-white p-6 rounded-lg flex flex-col items-center justify-center gap-6"
               initial={{ scale: 0.8, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
               exit={{ scale: 0.8, opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeInOut", delay: 0.1 }} 
+              transition={{ duration: 0.3, ease: "easeInOut", delay: 0.1 }}
             >
               <Typography.Text>Kindly subscribe to have complete access</Typography.Text>
 
