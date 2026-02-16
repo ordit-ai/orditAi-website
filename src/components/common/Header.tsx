50a394d7bb49f67417de7a6b98487f315fa738f4
mac <mac@MacBook-Pro.local>
2026-02-16 13:51:32 +0100

    clean

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -31,10 +31,10 @@ const Header = () => {
       name: "Pricing",
       path: APP_ROUTES.PRICING,
     },
-    {
-      name: "Contact Us",
-      path: APP_ROUTES.CONTACT,
-    },
+    // {
+    //   name: "Contact Us",
+    //   path: APP_ROUTES.CONTACT,
+    // },
   ];
 
   const checkArray = (array: string[]) => {
@@ -68,8 +68,8 @@ const Header = () => {
 
         <div className="md:flex hidden items-center space-x-6">
           <Link to="/">
-            <Typography.SubText className={cn(checkArray(locationArray) ? "text-black" : "text-white")}>
-              Login
+            <Typography.SubText className={ "text-black"}>
+              Sign In
             </Typography.SubText>
           </Link>
 
